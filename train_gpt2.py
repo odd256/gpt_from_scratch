b6a8bc21fa95f13b815e414fd286b3b52268dd55
odd <1211350624@qq.com>
2025-01-31 18:19:29 +0800

    section 2 gradient accum finished

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,3 +1,4 @@
+import inspect
 import time
 import sys
 import tiktoken
@@ -190,6 +191,30 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
         return model
 
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+        param_dict = {pn:p for pn, p in self.named_parameters()}
+        param_dict = {pn:p for pn, p in param_dict.items() if p.requires_grad}
+
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f'num decay parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters')
+        print(f'num nodecay parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters')
+        
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        print(f'using fused AdamW: {use_fused}')
+        
+        optim = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optim
+
 
 # -----------------------------------------------------------------------------
 
@@ -230,7 +255,20 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
     device = 'mps'
 print(f'using device: {device}')
 
-train_loader = DataLoaderLite(B=8, T=128)
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(1337)
+
+total_batch_size = 16*1024 # 2**19, ~0.5M tokens
+B = 8
+T = 128
+assert total_batch_size % (B*T) == 0, "total_batch_size must be divisible by B*T"
+grad_accum_steps = total_batch_size // (B*T)
+print(f'total desired batch size: {total_batch_size}')
+print(f'=> calculated gradient accumulation steps: {grad_accum_steps}')
+
+train_loader = DataLoaderLite(B=B, T=T)
 # set tf32
 torch.set_float32_matmul_precision('high')
 
@@ -238,49 +276,51 @@ model = GPT(config=GPTConfig(vocab_size=50304))
 model.to(device)
 # model = torch.compile(model)
 
-optim = AdamW(model.parameters(), lr=3e-4)
-for i in range(50):
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 20
+
+
+def get_lr(it):
+
+    # 1) linear warmup for the first 10 steps
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) min_lr for the rest of the steps
+    if it > max_steps:
+        return min_lr
+    # 3) cosine decay for the training steps
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+# optimize
+optim = model.configure_optimizers(weight_decay=0.1, learning_rate=max_lr, device=device)
+# optim = AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
+for step in range(max_steps):
     t0 = time.time()
-    x, y = train_loader.next_batch()
-    x, y = x.to(device), y.to(device)
-
     optim.zero_grad()
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits, loss = model(x, y)
+    loss_accum = 0.0
+    for micro_step in range(grad_accum_steps):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
 
-    loss.backward()
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        loss = loss / grad_accum_steps
+        loss_accum += loss.detach()
+        loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # set learning rate
+    lr = get_lr(step)
+    for param_group in optim.param_groups:
+        param_group['lr'] = lr
     optim.step()
     torch.cuda.synchronize()  # only needed with CUDA
     t1 = time.time()
     dt = (t1 - t0) * 1000  # ms
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1-t0)
     print(
-        f'step {i}, loss: {loss.item()}, dt: {dt:.2f} ms, tok/ms: {tokens_per_sec:.2f}')
-
-sys.exit(0)
-
-# prefix tokens
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("hello, i'm a language model,")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to(device)
-
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-
-while x.size(1) < max_length:
-    with torch.no_grad():
-        logits = model(x)  # (b, t, vocab_size)
-        logits = logits[:, -1, :]  # (b, vocab_size)
-        probs = f.softmax(logits, dim=-1)  # (b, vocab_size)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)  # (b, k)
-
-        ix = torch.multinomial(topk_probs, num_samples=1)  # (b, 1)
-        xcol = torch.gather(topk_indices, dim=-1, index=ix)  # (b, 1)
-        x = torch.cat((x, xcol), dim=-1)  # (b, t+1)
-
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+        f'step {step:4d}, loss: {loss_accum.item():.6f}, lr: {lr:.4e}, norm: {norm:.4f}, dt: {dt:.2f} ms, tok/ms: {tokens_per_sec:.2f}')
